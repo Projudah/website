46324ebde6f79a6bb00641eec2b8a7f905be9769
projudah@gmail.com <projudah@gmail.com>
2017-06-06 16:57:00 +0000

    Updated background

--- index.php
+++ index.php
@@ -3,8 +3,18 @@
 <head>
 <link rel="stylesheet" type="text/css" href="style.css">
 	<title>Home Page</title>
+	<p class="head">pro</p>
 </head>
 <body>
-<p>Hey Cath, Why you watching DSTV, LOL</p>
+<div class="notification">
+	<p>Hey Cath, Progress is made, i listened to the Love story song <br> its Great</p>
+</div>
+<div class = "buttons">
+	<ul class = "buttons_ul">
+		<li id="Games">Games</li>
+		<li id="Music">Music</li>
+		<li id="Art">Art</li>
+	</ul>
+</div>
 </body>
 </html>
