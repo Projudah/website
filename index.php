3835b2db29abb092e88ae61aac17954a34ed88b9
projudah@gmail.com <projudah@gmail.com>
2017-06-20 14:18:32 +0000

    Aesthetic Changes

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>Home Page</title>
-	<p class="head">pro</p>
+	<p class="head">PRO</p>
 </head>
 <body>
-<div class="notification">
-	<p>Hey Cath, Progress is made, i listened to the Love story song <br> its Great</p>
-</div>
-<div class = "buttons">
-	<ul class = "buttons_ul">
-		<li id="Games">Games</li>
-		<li id="Music">Music</li>
-		<li id="Art">Art</li>
-	</ul>
-</div>
+	<div class="notification">
+		<p>Hey Cath, Progress is made, i listened to the Love story song <br> its Great</p>
+	</div>
+	<div class = "buttons">
+		<ul class = "buttons_ul">
+		<a href="games"><li id="Games">Games</li></a>
+			<a href="music"><li id="Music">Music</li></a>
+			<a href="art"><li id="Art">Art</li></a>
+		</ul>
+	</div>
 </body>
 </html>
---
+++ music/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="mstyle.css">
+	<p class="head">PRO music</p>
+</head>
+<body>
+<div class = "albulm_art">
+	<div class="title">
+		B.O.E
+	</div>
+	<div class ="songs">
+		<p>What is Love</p>
+		<p>Empathy</p>
+		<p>Break</p>
+		<p>Nothing</p>
+	</div>
+</div>
+</body>
+</html>
