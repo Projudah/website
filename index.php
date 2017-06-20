4ce91326ca4265e6747ab070974e5ae6d058e397
projudah@gmail.com <projudah@gmail.com>
2017-06-20 15:12:43 +0000

    Moved things around

--- index.php
+++ index.php
@@ -3,15 +3,17 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>Home Page</title>
-	<p class="head">PRO</p>
 </head>
 <body>
-	<div class="notification">
-		<p>Hey Cath, Progress is made, i listened to the Love story song <br> its Great</p>
+	<div class = "bg">
+		<p class="head">PRO</p>
+		<div class="notification">
+			<p>Hey Cath, Progress is made, i listened to the Love story song <br> its Great</p>
+		</div>
 	</div>
 	<div class = "buttons">
 		<ul class = "buttons_ul">
-		<a href="games"><li id="Games">Games</li></a>
+			<a href="games"><li id="Games">Games</li></a>
 			<a href="music"><li id="Music">Music</li></a>
 			<a href="art"><li id="Art">Art</li></a>
 		</ul>
